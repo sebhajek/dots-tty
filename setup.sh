2bca970d77767d7dc078d3598989a8929270d5c2
sebhajek <seb.hajek@gmail.com>
2024-02-02 22:17:12 +0100

    tmux config

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 mkdir -p ~/.config/foot;
 cp -ur ~/.setup-dots/dots/tty/foot/* ~/.config/foot;
+
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp -ur ~/.setup-dots/dots/tty/tmux/tmux.conf ~/
