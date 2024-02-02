1b6e2239918ad48b2e4855318c1e8e576904c4a2
sebhajek <seb.hajek@gmail.com>
2024-02-02 22:56:40 +0100

    fixed tmux plugins dir

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ sudo zypper -n in -tpackage --no-recommends foot tmux;
 mkdir -p ~/.config/foot;
 cp -ur ~/.setup-dots/dots/tty/foot/* ~/.config/foot;
 
-mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp -ur ~/.setup-dots/dots/tty/tmux/tmux.conf ~/
+rm -rf ~/.tmux/plugins;
+mkdir -p ~/.tmux/plugins;
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
+cp -ur ~/.setup-dots/dots/tty/tmux/tmux.conf ~/;
